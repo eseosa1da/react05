a6f99cff0253250b28c4eed11ee2cdcec41dbc65
Eseosa Otasowie <eseosa1da35@Eseosas-Air.ht.home>
2022-04-09 22:50:07 -0400

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
+
+
+
+
+
+
+
+export default  combineReducers ({
+
+    posts: postsReducer,
+    users: usersReducer
+});
