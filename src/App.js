40e4c591692c574dd4368eeecba6dc40e3ba6ae3
Eseosa Otasowie <eseosa1da35@Eseosas-Air.ht.home>
2022-04-09 22:27:45 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/welcome" />} />
+
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route
+              path="new-user"
+              element={<p>Welcome to the club new user!</p>}
+            />
+          </Route>
+
+          <Route path="/products" element={<Products />} />
+
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
